43b203beea8482f43e5730c821f3be54911d842d
Oleg Dudkin <dudkin.on@gmail.com>
2018-01-13 01:48:12 -0600

    feat: Add stringStartsWith function

--- js/app.js
+++ js/app.js
@@ -6,12 +6,22 @@ if (initMap !== undefined) var initMap;
   var map;
 
   var Worker = function () {
+
     var scope = this;
+
     scope.getLocations = function (func) {
       /**
        * Async function provide locations
        */
       $.getJSON('/js/locations.json', func)
+    };
+
+    scope.stringStartsWith = function (string, startsWith) {
+
+      string = string || "";
+      if (startsWith.length > string.length)
+        return false;
+      return string.substring(0, startsWith.length) === startsWith;
     };
   };
 
